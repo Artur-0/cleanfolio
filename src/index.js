e9e1b93428b7fd533c5bd84586afdb81c2e82466
Artur-0 <romeroarturo597@gmail.com>
2022-10-10 18:42:39 -0300

    fix deploy

--- src/index.js
+++ src/index.js
@@ -2,7 +2,11 @@ import { render } from 'react-dom'
 import App from './App'
 import { ThemeProvider } from './contexts/theme'
 import './index.css'
+// import axios from 'axios'
+// import dotenv from 'dotenv'
+// dotenv.config()
 
+// axios.defaults.baseURL = process.env.REACT_APP_API || 'http://localhost:3001'
 render(
   <ThemeProvider>
     <App />
